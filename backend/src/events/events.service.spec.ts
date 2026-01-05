edb43fd6471b8a1f807fa61b6649c7c7371a583f
Tomiwa Aluko <tomiwaaluko02@gmail.com>
2026-01-05 01:39:47 -0500

    fix: passed unit tests

--- backend/src/events/events.service.spec.ts
+++ backend/src/events/events.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventsService } from './events.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('EventsService', () => {
   let service: EventsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EventsService],
+      providers: [
+        EventsService,
+        {
+          provide: PrismaService,
+          useValue: {
+            event: {
+              create: jest.fn(),
+              findMany: jest.fn(),
+              findUnique: jest.fn(),
+              update: jest.fn(),
+              delete: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<EventsService>(EventsService);
