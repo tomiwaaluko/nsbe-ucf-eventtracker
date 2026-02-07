fde4e672a9e5c950a9478fce3276425a37b803e4
Aridsondez <schoolaridsondez@gmail.com>
2026-02-07 17:04:03 -0500

    feat[NSB-15 NSB-17 NSB-16 NSB-14] updated profile section

--- backend/src/members/members.module.ts
+++ backend/src/members/members.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MembersController } from './members.controller';
 import { MembersService } from './members.service';
 import { AuthModule } from '../auth/auth.module';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, StorageModule],
   controllers: [MembersController],
   providers: [MembersService],
   exports: [MembersService],
